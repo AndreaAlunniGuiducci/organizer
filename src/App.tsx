ef93a1a04c7aa07b1b43600b007be08c32684ae9
AndreaAlunniGuiducci <andrea.ag@gmail.com>
2025-04-24 17:32:37 +0200

    add: github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 import {
   Link,
   Outlet,
   RouterProvider,
-  createBrowserRouter,
   createHashRouter,
 } from "react-router-dom";
-import QRCode from "react-qr-code";
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
-import { toPng } from "html-to-image";
-import { Button, Card, Form, Image } from "react-bootstrap";
-import { Formik } from "formik";
-import * as yup from "yup";
-import { uploadToImgBB } from "./utils/uploadImage";
-import NewList from "./pages/newList/newList";
 import List from "./pages/list/list";
+import NewList from "./pages/newList/newList";
 
 function App() {
-  const [qrCodeValue, setQrCodeValue] = useState<string | undefined>();
-  const [formValue, setFormValue] = useState<
-    | {
-        id: number;
-        objectName: string;
-        objectType: string;
-        objectCustomType: string;
-        objectImage: File | null;
-        objectImageUrl: string;
-      }[]
-    | undefined
-  >();
-
-  console.log("FORMIK FORM VALUES", formValue);
-
-  const qrRef = useRef<HTMLDivElement>(null);
-
-  const handleDownload = () => {
-    if (qrRef.current === null) return;
-
-    toPng(qrRef.current)
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "qr-code.png";
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.error("Errore durante il download:", err);
-      });
-  };
-
-  const deleteObject = (id: number) => {
-    const newFormValue = formValue?.filter((i) => i.id !== id);
-    if (newFormValue === undefined || newFormValue.length === 0) {
-      setFormValue(undefined);
-      return;
-    }
-    setFormValue(newFormValue);
-  };
-
-  useEffect(() => {
-    if (formValue) {
-      const qrCodeData = formValue.map((item) => {
-        return {
-          objectName: item.objectName,
-          objectType: item.objectType,
-          objectCustomType: item.objectCustomType,
-          objectImageUrl: item.objectImageUrl,
-        };
-      });
-      setQrCodeValue(JSON.stringify(qrCodeData));
-    } else {
-      setQrCodeValue(undefined);
-    }
-  }, [formValue]);
-
   // da usare se non si utilizza formik
   // const [validated, setValidated] = useState(false);
   // const handleSubmit = (event: any) => {
