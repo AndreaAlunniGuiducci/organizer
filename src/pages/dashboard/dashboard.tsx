d85bb877e049e226a49fdbece9fa7a7ce92a1781
AndreaAlunniGuiducci <andrea.ag@gmail.com>
2025-05-10 00:36:17 +0200

    add dashboard

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <h3>I tuoi luoghi</h3>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
